1f962d4903b43a183651a26ddc1ec76215868951
jeffersonliverpool5-rgb <jefferson.liverpool5@gmail.com>
2026-01-21 09:52:41 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ def buscar_placares_simples():
     try:
         # Livescore.bz é um site bem básico, fácil de ler
         print("Acessando site de placares...")
-        driver.get("http://www.livescore.bz/live.aspx") 
+        driver.get("https://www.livescore.com/pt/") 
         time.sleep(10) # Tempo para os placares carregarem
 
         # No livescore.bz, os jogos ficam em linhas de tabela (tr)
