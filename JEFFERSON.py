79a0731c98f80a8f3b6f41a18cee7f7e51ac0225
jeffersonliverpool5-rgb <jefferson.liverpool5@gmail.com>
2026-01-28 14:15:40 -0300

    Update JEFFERSON.py

--- JEFFERSON.py
+++ JEFFERSON.py
@@ -31,7 +31,7 @@ def extrair_aiscore():
     
     try:
         # URL do jogo que você enviou por último
-        url = "https://www.aiscore.com/match-talleres-cordoba-club-atletico-newell's-old-boys/ezk96i3g42zf1kn/h2h"
+        url = "https://www.aiscore.com/br/match-arsenal-women-as-far-rabat-women/wv78xivmoxraokr/h2h"
         driver.get(url)
         
         # Espera para carregar o conteúdo dinâmico
@@ -42,7 +42,7 @@ def extrair_aiscore():
             time_casa = driver.find_element(By.XPATH, "//div[contains(@class, 'home-team')]//a[contains(@class, 'name')]").text.strip()
             time_fora = driver.find_element(By.XPATH, "//div[contains(@class, 'away-team')]//a[contains(@class, 'name')]").text.strip()
         except:
-            time_casa, time_fora = "Talleres Cordoba", "Club Atlético Newell's Old Boys"
+            time_casa, time_fora = "Arsenal L.F.C. (Feminino)", "AS FAR Rabat Women"
 
         # 2. Busca o Placar (Gols)
         try:
